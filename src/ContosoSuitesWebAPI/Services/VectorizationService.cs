d559d388687b8a41483270bb23445179f1a1e6df
FvanEgmond <fvanegmond@hotmail.com>
2024-11-13 18:05:32 +0000

    Excercise 5.1 2nd try

--- src/ContosoSuitesWebAPI/Services/VectorizationService.cs
+++ src/ContosoSuitesWebAPI/Services/VectorizationService.cs
@@ -13,7 +13,7 @@ namespace ContosoSuitesWebAPI.Services
     
     public class VectorizationService(Kernel kernel, CosmosClient cosmosClient, IConfiguration configuration) : IVectorizationService
     {
-        //private readonly AzureOpenAIClient _client = openAIClient;
+
         private readonly Kernel _kernel = kernel;
         private readonly CosmosClient _cosmosClient = cosmosClient;
         private readonly string _embeddingDeploymentName = configuration.GetValue<string>("AzureOpenAI:EmbeddingDeploymentName") ?? "text-embedding-ada-002";
@@ -27,8 +27,9 @@ namespace ContosoSuitesWebAPI.Services
             // var embeddingClient = _client.GetEmbeddingClient(_embeddingDeploymentName);
             try
             {
-                // Generate a vector for the provided text.
+                
                 #pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+                // Generate a vector for the provided text.
                 var embeddings = await _kernel.GetRequiredService<ITextEmbeddingGenerationService>().GenerateEmbeddingAsync(text);
                 #pragma warning restore SKEXP0001
 
